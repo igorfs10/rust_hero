7ed5da8768a095ec6e470f9a261fde9706678f78
igorfs10 <igorfs10@gmail.com>
2020-11-01 22:44:19 -0300

    or

--- rust_hero_dados/src/sistema_batalha.rs
+++ rust_hero_dados/src/sistema_batalha.rs
@@ -5,11 +5,13 @@ use crate::consts::MULTIPLICADOR_CRITICO;
 
 pub fn atacar(ataque: &u8, defesa: &u8, vida: &mut u8, numero_rng: &u64) {
     let mut rng: StdRng = SeedableRng::seed_from_u64(*numero_rng);
+    let mut dano;
     let critico = rng.gen_ratio(1, 3);
+
     if critico {
         println!("Golpe crítico");
     }
-    let mut dano;
+
     if *ataque <= *defesa {
         dano = 1;
     } else {
