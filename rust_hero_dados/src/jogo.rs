3d69afcc061b36a8a9f43d957880bce7346331cb
igorfs10 <igorfs10@gmail.com>
2020-11-07 00:06:28 -0300

    mais uma funcao

--- rust_hero_dados/src/jogo.rs
+++ rust_hero_dados/src/jogo.rs
@@ -1,4 +1,6 @@
 use crate::dados::equipamentos::Equipamentos;
+use crate::dados::inimigos::Inimigos;
+use crate::structs::personagem::Personagem;
 
 pub fn match_equipamento(id_equipamento: usize) -> Equipamentos {
     match id_equipamento {
@@ -7,4 +9,16 @@ pub fn match_equipamento(id_equipamento: usize) -> Equipamentos {
         3 => Equipamentos::Bastao,
         _ => Equipamentos::Nenhum,
     }
+}
+
+pub fn escolher_inimigo(inimigo: &Inimigos) -> Personagem {
+    let mut personagem = Personagem::default();
+    let inimigo = inimigo.get_inimigo();
+    personagem.nome = inimigo.nome.to_string();
+    personagem.ataque = inimigo.ataque;
+    personagem.defesa = inimigo.defesa;
+    personagem.vida_total = inimigo.vida;
+    personagem.vida_atual = inimigo.vida;
+    personagem.experiencia = inimigo.experiencia;
+    personagem
 }
